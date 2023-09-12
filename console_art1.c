65d667200f319c1a6d2a109d608fb60f59a1a6dd
Narendra Hindwar <hindwarnarendra@gmail.com>
2023-09-13 05:00:24 +0530

    formatted to not go out of bounds

--- console_art1.c
+++ console_art1.c
@@ -2,8 +2,10 @@
 #include<string.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<time.h>
+#include<math.h>
 #define microsecond 1000
-#define Max_size 39
+#define Max_size 40
 
 
 /* Funtion to animate the string laterally */ 
@@ -24,16 +26,16 @@ int main(int argc, char *argv[]){
         printf("Please Enter the word to animate\n");
         return 1;
     }
+
+    srand(time(0));
     //variable decleration
     int size,direction,pivit,a;
     a=-2;                                               //downword movement step
-    pivit=10;                                           //initial position
+    pivit=floor((rand()/RAND_MAX)*16)+2;                                           //initial position
     direction=1;                                        //direction of the lateral movement
 
     char* name;
     name=(char*)calloc(Max_size,sizeof(char));          //string that stores the passed argument
-    char* output;
-    output=(char*)calloc(200,sizeof(char));             //frame string 
 
     //Storing the string and appending the empty spaces around it
     strcpy(name,argv[1]);
@@ -46,37 +48,28 @@ int main(int argc, char *argv[]){
     strcat(name,space);
 
     printf("\033[H\033[J");
-    printf(" _______________________________________ \n");
+    printf(" ________________________________________ \n");
         
         for(int i=1;i<=21;i++)
-             printf("|                                       |\n");
-    printf("|---------------------------------------|\n");
+             printf("|                                        |\n");
+    printf("|----------------------------------------|\n");
 
     while(1){
-        printf("\033[?25l");   
+        printf("\033[?25l");                            //hiding the cursor   
         printf("\033[%d;1H\033[K",pivit+2);
-        printf("|                                       |");                         //hiding the cursor                   
+        printf("|                                        |");                                         
         pivit=pivit+a;                                  //pivit increment or decrement
         if(pivit>=20|| pivit<=1)                        // change the pivit direction once it reaches an extreme
         a*=-1;
         printf("\033[%d;2H\033[K",pivit+2);
         printf("%s|",name);
 
-        // Storing the output data in a single frame to reduce flicker
-        
-        //strcat(output,"|");
-        //strcat(output,name);
-        //strcat(output,"|\n");
-        //printf("%s",output);
-        //printf("\033[?25h");
-        //animate the next frame
         animate(name,direction);
         if(name[0]!=' ')
             direction=1;
         if(name[Max_size-2]!=' ')
             direction=-1;
-        usleep(100*microsecond);
-        output[0]='\0';                                  //clearing the output buffer 
+        usleep(100*microsecond);                                  //clearing the output buffer 
     }
 
 }
