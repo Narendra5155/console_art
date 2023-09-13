bee25c49ec9e380f000617afd4bc38c2510d759e
Narendra Hindwar <hindwarnarendra@gmail.com>
2023-09-13 05:55:55 +0530

    Added resizable rows and columns

--- console_art1.c
+++ console_art1.c
@@ -3,69 +3,79 @@
 #include<unistd.h>
 #include<stdlib.h>
 #define microsecond 1000
-#define Max_size 40
+#define Row_size 80
+#define Column_size 27
 
-
-/* Funtion to animate the string laterally */ 
-void animate(char* text,int direction){
-    int length=strlen(text);
-    char temp[length+1];
-    strcpy(temp,text);
-    for(int i=0;i<length;i++){
-        if(direction>0)
-        text[(i+2)%length]=temp[i];
-        else if(direction<0)
-        text[i]=temp[(i+1)%length];
-    }
+void printHelp(){
+    printf("The syntax is \n .\\console_art1 \"YOUR TEXT TO ANIMATE\"");
+    printf("\nUse the -h or -help flag to print this messeage\n");
 }
+
+void printBox(){
+    printf("\033[H\033[J");
+    printf(" ");
+        for(int i=1;i<=Row_size+1;i++)
+            printf("_");
+    printf(" \n");
+        
+        for(int i=1;i<=Column_size;i++){
+             printf("|");
+             for(int j=0;j<=Row_size;j++)
+                printf(" ");
+             printf("|\n");
+        }
+    printf("|");
+        for(int i=0;i<=Row_size;i++)
+            printf("-");
+    printf("|\n");
+}
+
 int main(int argc, char *argv[]){
     //Check if an argument is passed or not
     if(argv[1]==NULL){
-        printf("Please Enter the word to animate\n");
+        printf("No argument");
         return 1;
     }
+    if(strcmp(argv[1],"-h")==0|| strcmp(argv[1],"--help")==0){
+        printHelp();
+        return 0;
+        }
 
     //variable decleration
-    int size,direction,pivit,a;
-    a=-2;                                               //downword movement step
+    int size,direction,pivit,a,b;
+    a=-2;
+    b=2;                                               //downword movement step
     pivit=9;                                           //initial position
     direction=1;                                        //direction of the lateral movement
 
     char* name;
-    name=(char*)calloc(Max_size,sizeof(char));          //string that stores the passed argument
+    name=(char*)calloc(strlen(argv[1])+1,sizeof(char));          //string that stores the passed argument
 
     //Storing the string and appending the empty spaces around it
     strcpy(name,argv[1]);
+    char* mask;
+    mask=(char*)calloc(strlen(name)+1,sizeof(char));
+    for(int i=0;i<strlen(name);i++)
+    mask[i]=' ';
+    mask[strlen(name)]='\0';
     size=strlen(name);
-    int empty_space=Max_size-size;
-    char space[empty_space+1];
-    for(int i=0;i<empty_space;i++)
-    space[i]=' ';
-    space[empty_space]='\0';
-    strcat(name,space);
-
-    printf("\033[H\033[J");
-    printf(" ________________________________________ \n");
-        
-        for(int i=1;i<=21;i++)
-             printf("|                                        |\n");
-    printf("|----------------------------------------|\n");
+    size=Row_size-size;
+    
+    printBox();
 
     while(1){
         printf("\033[?25l");                            //hiding the cursor   
-        printf("\033[%d;1H\033[K",pivit+1);
-        printf("|                                        |");                                         
+        printf("\033[%d;%dH",pivit+1,direction+1);
+        printf("%s",mask);                                         
         pivit=pivit+a;                                  //pivit increment or decrement
-        if(pivit>=21|| pivit<=1)                        // change the pivit direction once it reaches an extreme
+        if(pivit>=Column_size|| pivit<=1)                        // change the pivit direction once it reaches an extreme
         a*=-1;
-        printf("\033[%d;2H\033[K",pivit+1);
-        printf("%s|",name);
-
-        animate(name,direction);
-        if(name[0]!=' ')
-            direction=1;
-        if(name[Max_size-2]!=' ')
-            direction=-1;
+        direction=direction+b;
+        if(direction>=size || direction<=1)
+        b*=-1;
+        printf("\033[%d;%dH",pivit+1,direction+1);
+        printf("%s",name);
+        
         usleep(100*microsecond);                                  //clearing the output buffer 
     }
 
